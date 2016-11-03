023df3da3483350645ea4478116ff6663f4c3014
Franklin Schrans <f.schrans@me.com>
2016-11-03 10:17:32 +0000

    Add Step 2

--- Set 2.hs
+++ Set 2.hs
@@ -6,3 +6,26 @@ data Maybe a = Nothing | Just a
 instance Show a => Show (Maybe a) where
   show Nothing = "Nothing"
   show (Just a) = "Just " ++ show a
+
+headMay :: [a] -> Maybe a
+headMay (x : _) = Just x
+headMay [] = Nothing
+
+tailMay :: [a] -> Maybe [a]
+tailMay (_ : xs) = Just xs
+tailMay [] = Nothing
+
+lookupMay :: Eq a => a -> [(a,b)] -> Maybe b
+lookupMay v ds = headMay $ map snd $ filter ((==) v.fst) ds
+
+divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
+divMay _ 0 = Nothing
+divMay x y = Just (x / y)
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay [] = Nothing 
+maximumMay xs = Just (foldr1 (max) xs)
+
+minimumMay :: Ord a => [a] -> Maybe a
+minimumMay [] = Nothing 
+minimumMay xs = Just (foldr1 (min) xs)
