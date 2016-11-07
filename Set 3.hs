b431e36b2d87ea52ed18436a3888123a3b8b544c
Franklin Schrans <f.schrans@me.com>
2016-11-07 12:32:21 +0000

    Add Poker Hands

--- Set 3.hs
+++ Set 3.hs
@@ -4,3 +4,13 @@ allPairs :: [a] -> [b] -> [(a,b)]
 allPairs [] _ = []
 allPairs _ [] = []
 allPairs (x:xs) ys = (map ((,) x) ys) ++ allPairs xs ys
+
+data Card = Card Int String
+
+instance Show Card where
+  show (Card a b) = show a ++ b
+
+allCards :: [Int] -> [String] -> [Card]
+allCards [] _ = []
+allCards _ [] = []
+allCards (x:xs) ys = (map (Card x) ys) ++ allCards xs ys
