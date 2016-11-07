2bf57635e0d719b70c6ad64e9ab3fcb4a0f6d70e
Franklin Schrans <f.schrans@me.com>
2016-11-07 21:30:06 +0000

    Implement combStep

--- Set 3.hs
+++ Set 3.hs
@@ -14,3 +14,20 @@ allCards = allCombs Card
 allCombs :: (a -> b -> c) -> [a] -> [b] -> [c]
 allCombs _ [] _ = []
 allCombs f (x:xs) ys = (map (f x) ys) ++ allCombs f xs ys
+
+allCombs3 :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
+allCombs3 _ [] _ _ = []
+allCombs3 f (x:xs) ys zs = 
+    concat (map (\g -> map g zs) (map (f x) ys)) ++ 
+        (allCombs3 f xs ys zs)
+
+combStep :: [a -> b] -> [a] -> [b]
+combStep [] _ = []
+combStep _ [] = []
+combStep (f:fs) as = map f as ++ combStep fs as
+
+allCombs' :: (a -> b -> c) -> [a] -> [b] -> [c]
+allCombs' f xs ys = combStep (map f xs) ys
+
+allCombs3' :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
+allCombs3' f xs ys zs = combStep (combStep (map f xs) ys) zs
