3bc38613cf9df257b0448803b74d60f1687d1344
Franklin Schrans <f.schrans@me.com>
2016-11-10 17:45:36 +0000

    Rewrite Set 1

--- Set 4.hs
+++ Set 4.hs
@@ -1,6 +1,7 @@
 import Prelude()
 import MCPrelude
 import Set2
+import Control.Arrow((&&&))
 
 class Monad m where
   bind :: m a -> (a -> m b) -> m b
@@ -48,3 +49,60 @@ join = flip bind id
 
 ap :: Monad m => m (a -> b) -> m a -> m b
 ap f a = (liftM2 id) f a
+
+-- Set 1
+
+fiveRands :: [Integer]
+{-fiveRands = take 5 (tail (map fst (iterate (rand . snd) (0, mkSeed 1))))-}
+fiveRands = evalGen (sequence (take 5 (repeat (Gen rand)))) (mkSeed 1)
+
+randString3 :: String
+{-randString3 = take 3 (tail (map fst (iterate (randLetter . snd) ('0', mkSeed 1))))-}
+randString3 = evalGen (sequence (take 3 (repeat randLetter))) (mkSeed 1)
+
+generalA :: (a -> b) -> Gen a -> Gen b
+generalA f g = (=<<) (return . f) g
+
+generalB :: (a -> b -> c) -> Gen a -> Gen b -> Gen c
+generalB = liftM2
+
+{-generalB2 :: (a -> b -> c) -> Gen a -> Gen b -> Gen c-}
+{-generalB2 f ga gb = -}
+    {-genTwo ga (\aa -> -}
+            {-genTwo gb (\bb ->-}
+                        {-mkGen $ f aa bb))-}
+
+{-generalPair2' :: Gen a -> Gen b -> Gen (a, b)-}
+{-generalPair2' = generalB2 (,)-}
+
+{-randPair'' = generalPair2' randLetter rand-}
+
+repRandom2 :: [Gen a] -> Gen [a]
+repRandom2 = sequence
+
+generalPair2 :: Gen a -> Gen b -> Gen (a,b)
+generalPair2 = liftM2 (,)
+
+{-repRandom :: [Gen a] -> Gen [a]-}
+{-repRandom = ((.).(.)) (reverse . fst &&& snd) (flip (.) (mkGen []) . (flip (foldr (ap (flip (&&&) snd . (flip (.) fst . (flip (:) . fst))) . (flip (.) snd)))))-}
+{---repRandom gens s = foldr gens-}
+
+mkGen :: a -> Gen a
+mkGen = return
+
+genTwo :: Gen a -> (a -> Gen b) -> Gen b
+genTwo = bind
+
+randLetter :: Gen Char
+randLetter = generalA toLetter (Gen rand)
+    
+randEven :: Gen Integer
+randEven = generalA (*2) (Gen rand)
+
+randOdd :: Gen Integer
+randOdd = generalA (+1) randEven
+
+randTen :: Gen Integer
+randTen = generalA (*10) (Gen rand)
+
+randPair = generalPair2 randLetter (Gen rand)
