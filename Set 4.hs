3a1a3453fd3060cdb8183a49134846f52c5906d0
Franklin Schrans <f.schrans@me.com>
2016-11-10 17:04:51 +0000

    Implement ex 5

--- Set 4.hs
+++ Set 4.hs
@@ -22,3 +22,29 @@ instance Monad Gen where
 
 evalGen :: Gen a -> Seed -> a
 evalGen g s = fst (runGen g s)
+
+sequence :: Monad m => [m a] -> m [a]
+sequence [] = return []
+sequence (g:gs) = bind g (\g' ->
+                  bind (sequence gs) (\gs' ->
+                  return (g':gs')))
+
+liftM2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+liftM2 f ga gb = bind ga (\aa -> 
+                 bind gb (\bb ->
+                 return $ f aa bb))
+
+liftM3 :: Monad m => (a -> b -> c -> d) -> m a -> m b -> m c -> m d
+liftM3 f ma mb mc = bind ma (\a ->
+                    bind mb (\b ->
+                    bind mc (\c ->
+                    return $ f a b c)))
+
+(=<<) :: Monad m => (a -> m b) -> m a -> m b
+(=<<) = flip bind
+
+join :: Monad m => m (m a) -> m a
+join = flip bind id
+
+ap :: Monad m => m (a -> b) -> m a -> m b
+ap f a = (liftM2 id) f a
